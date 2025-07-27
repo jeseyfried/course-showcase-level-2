1f0a65d67af7f7cf9a2b1f21757a3e15ab4eb2b3
jeseyfried <jseyfried@unm.edu>
2025-07-27 13:09:49 -0600

    Add files via upload

---
+++ assets/js/directory.js
@@ -0,0 +1,67 @@
+"use strict";
+
+/* ==============================
+ * Google Sheets interface
+ * ==============================
+ */
+
+function getPages(callback) {
+  console.log($('#sheetrock').html());
+  console.log("starting getPages");
+    // The spreadsheet must be either "visible to anyone with the link", or "public on the web".
+
+    //https://docs.google.com/spreadsheets/d/1x-7ms5fDNzW2Ah9AXylV0ERWh4O-YlwVpguptmWlWbU/edit?usp=sharing#gid=0;
+    var sheetURL =
+    "https://docs.google.com/spreadsheets/d/1pP8KhM3E5rmA3ce2Q3oKNFmqJHnEuLT15Nkp_NI61u8/edit?usp=sharing#gid=0"
+    var pages = [];
+    $('#sheetrock').sheetrock({
+        url: sheetURL,
+        query: "select *",
+        fetchSize: 0,
+        reset: true,
+        target: document.getElementById("sheetrock"),
+        rowTemplate: function(row) {
+            //console.log(row);
+            var m = row.cellsArray;
+            m.title = m[0];
+            m.description = m[2];
+            m.filename = m[3];
+            m.image = m[4];
+            //m.categories = m[4].split(",");
+            //console.log(m.contentPages);
+            pages.push(m);
+            console.log("Row " + row.num + " is OK.");
+            return $('<span></span>'); // appease the sheetrock table handler
+        },
+        callback: function (error, options, response) {
+            console.log("Retrieved " + pages.length + " rows.");
+            //console.log(error, options, response);
+
+            // when data is done loading, execute supplied callback function
+            callback(pages);
+        }
+    });
+}
+
+function createCard(i,page) {
+
+  //loop through supplied slugs to retrieve page title and subtitle
+  console.log("about to create card.");
+
+    var filename = page.filename + '.html';
+    console.log("processing " + filename);
+
+    var rowString = "";
+    rowString += '<div class="col">';
+    rowString += '<div class="card v-card">';
+    rowString += '<a href="sites/'+filename+'">';
+    rowString += '<img class="card-img-top" src="/assets/images/' + page.image + '" alt="Essay image">';
+    rowString += '<div class="card-body">';
+    rowString += '<h3 class="card-title">' + page.title + '</h3>';
+    rowString += '<p class="card-text">' + page.description + '</p>';
+    rowString += '</div></a></div></div>';
+
+    console.log("just made row: " + rowString);
+    
+    $('.cards').append(rowString);
+}
